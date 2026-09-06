fb12c072f3ac1d6626f8242962e171c69750a73e
Krishnawasthi <km.awasthi.77@gmail.com>
2026-09-06 20:24:17 +0530

    multithreading

---
+++ Multi_Threading/src/com/multithreading/thread/day1/HelloWord.java
@@ -0,0 +1,12 @@
+package com.multithreading.thread.day1;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Who is running this thread: "+Thread.currentThread().getName());
+		System.out.println("HelloWord.main()");
+
+	}
+
+}
