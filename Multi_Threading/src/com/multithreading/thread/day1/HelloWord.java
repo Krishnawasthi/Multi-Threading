ce8e94e77d927735341845928b08d9bc1c896f65
Krishnawasthi <km.awasthi.77@gmail.com>
2026-09-06 20:32:21 +0530

    asfad

--- Multi_Threading/src/com/multithreading/thread/day1/HelloWord.java
+++ Multi_Threading/src/com/multithreading/thread/day1/HelloWord.java
@@ -5,11 +5,17 @@ public class HelloWord {
 	public static void main(String[] args) {
 		
 		System.out.println("Who is running this thread: "+Thread.currentThread().getName());
-		Thread.currentThread().setName(" My Thread");
 		
-		System.out.println("Who is running this thread: "+Thread.currentThread().getName());
 		System.out.println("HelloWord.main()");
+		
+		HelloWord hw = new HelloWord();
+		hw.someMethod();
 
+	}
+	
+	public void someMethod() {
+		System.out.println("Who is running this thread: "+Thread.currentThread().getName());
+		System.out.println("HelloWord.someMethod()");
 	}
 
 }
